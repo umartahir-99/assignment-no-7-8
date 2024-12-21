a57d6113c6810ecb31aabed68693309e86fe59df
umartahir-99 <tahirumar835@gmail.com>
2024-12-21 20:26:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Simulate a coin toss.
+// Task:
+// Ask the user to input 1 for "Heads" or 0 for "Tails."
+// Use the random module to simulate a coin toss (random.randint(0, 1)).
+// Compare the user’s input with the result and print "You Win!" or "You lose.
+var number = prompt("Toss a coin:");
+if(number == 1){
+   console.log("You Win")
+}
+else if(number == 0){
+   console.log("You Lose")
+}
+else{
+  console.log("invalid input")
+}
+
+
+
+
+
+
